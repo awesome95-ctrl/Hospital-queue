b473181d49c32bc514cdbbf9d7493da50da74048
awesome95-ctrl <awesomecharles247@gmail.com>
2026-08-17 08:52:02 +0100

    Fix startt.sh to run DatabaseSeeder instead of DepartmentSeeder

--- docker/start.sh
+++ docker/start.sh
@@ -3,7 +3,7 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
-php artisan db:seed --class=DepartmentSeeder --force
+php artisan db:seed --class=DatabaseSeeder --force
 
 service nginx start
 php-fpm
